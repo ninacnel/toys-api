188e1fba5d964cfdaf24d4a58325bbc3baed35f8
ninacnel <nerinacoronel00@gmail.com>
2023-11-29 08:22:50 -0300

    Increase Stock by 50

---
+++ Repository/StockRepository.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Data.DTOs;
+using Data.Mappings;
+using Data.Models;
+
+namespace Repository
+{
+    public class StockRepository
+    {
+        private readonly toystoreContext _context;
+        private readonly IMapper _mapper;
+
+        public StockRepository(toystoreContext context)
+        {
+            _context = context;
+            _mapper = AutoMapperConfig.Configure();
+        }
+
+        public ToyDTO AddStock(int toycode)
+        {
+            var toyDB = _context.toys.Single(t => t.code == toycode);
+
+            toyDB.stock += 50;
+
+            var toyDTO = _mapper.Map<ToyDTO>(toyDB);
+
+            _context.SaveChanges();
+
+            return toyDTO;
+        }
+    }
+}
---
+++ Services/Services/StockService.cs
@@ -0,0 +1,21 @@
+﻿using Data.DTOs;
+using Repository;
+using Services.IServices;
+
+namespace Services.Services
+{
+    public class StockService : IStockService
+    {
+        private readonly StockRepository _repository;
+
+        public StockService(StockRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ToyDTO AddStock(int toycode)
+        {
+            return _repository.AddStock(toycode);
+        }
+    }
+}
